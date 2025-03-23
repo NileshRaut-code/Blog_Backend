1cb2863904f7c0863abde67b025c65c2bf8e2cee
Nilesh Raut <nileshraut898@gmail.com>
2025-03-23 11:51:03 +0530

    Minor fixe

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -2,7 +2,7 @@ import { Post } from "../models/blog.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-
+import { User } from "../models/user.model.js";
 export const GetAllpending=asyncHandler(async(req,res)=>{
     const {pstate}=req.params
     if(!["approved", "rejected", "pending"].includes(pstate)){
